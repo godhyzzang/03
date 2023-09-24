e47eec9a6060b123b60c9889c0f5153f31130592
godhyzzang <amksh0914@gmail.com>
2023-09-24 23:10:08 +0900

    Update main.c
    1

--- main.c
+++ main.c
@@ -2,6 +2,20 @@
 
 
 int main(int argc, char *argv[]) {
-	printf("Hello");
+	// printf("Enter a float : ");
+	int i=0;
+	float f = 0.0;
+
+	printf("enter an integer\n");
+
+	//scanf_s("enter an integer : %d", &i);
+	scanf_s("%d", &i);
+
+	printf("enter a float\n");
+	//scanf_s("enter a float number : %f", &f);
+	scanf_s("%f", &f);
+	
+
+	printf("integer number : %d, float number : %f", i, f);
 	return 0;
 }
